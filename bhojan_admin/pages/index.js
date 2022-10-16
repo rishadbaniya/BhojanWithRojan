b8ef63464eec80864131c819181b419a88a70d7f
rishadbaniya <rbdroid01@gmail.com>
2022-10-16 15:31:26 +0545

    Few Updates on Admin

--- bhojan_admin/pages/index.js
+++ bhojan_admin/pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import {QWebChannel} from 'qwebchannel';
+
+import {useState, useEffect} from 'react';
 
 const DEVELOPERS = [
   "Rishad Baniya",
@@ -7,6 +10,10 @@ const DEVELOPERS = [
   "Kapil Adhikari",
   "Rojan Gautam"
 ];
+
+let X = 0;
+
+
 
 const DevelopedBy = () =>{
   return <>
@@ -21,35 +28,83 @@ const DevelopedBy = () =>{
     </>
 }
 
-const EnterUsername = () => {
+const EnterUsername = (props) => {
    return <div className="enterusername_wrapper">
-        <input class="inputbox" placeholder="Enter your username"/>
+        <input onChange={props.onChange} class="inputbox" placeholder="Enter your username"/>
      </div>
 }
 
-const EnterPassword = () => {
+
+const EnterPassword = (props) => {
    return <div className="enterpassword_wrapper">
-        <input class="inputbox" placeholder="Enter your password" />
+        <input onChange={props.onChange}className="inputbox" placeholder="Enter your password" />
      </div>
 }
 
-const LoginButton = () => {
+const LoginButton = (props) => {
    return <div className="loginbutton_wrapper">
-       <div className="login__login_button">Login</div>
+     <div onClick={props.onClick} className="login__login_button">Login</div>
     </div>
 
 }
 
+// TODO : Implement the callback for the transport portion of the QOBject
+const loginClick = (username, password) =>{
+
+    // Sending the SHA-256 Hash of the password to the Qt Business Logic Section 
+  window.qt_object.sendLoginCredentials(`
+  {
+     "username" : ${username},
+     "password" : ${password}
+  }
+  `);
+}
+
 const Login = () => {
+  const [username, updateUsername] = useState('');
+  const [password, updatePass] = useState('');
+
+  const handleOnChangePassword = (event) => {
+    console.log(event.target.value);
+    updateUsername(event.target.value);
+  }
+
+  const handleOnChangeUsername = (event) => {
+    console.log(event.target.value);
+    updatePass(event.target.value);
+  }
+
   return <div className="login">
       <div className="login__login_title">Admin Login</div>
-      <EnterUsername />
-      <EnterPassword />
-      <LoginButton />
+      <EnterUsername onChange={handleOnChangeUsername}/>
+      <EnterPassword onChange={handleOnChangePassword}/>
+      <LoginButton onClick={() => loginClick(username, password)}/>
    </div>
 }
 
 export default function Home() {
+
+  useEffect(()=>{
+    let socket = new WebSocket("ws://localhost:12345");
+    console.log("Came here");
+    socket.onerror = () => {
+        console.log("Fuck");
+    }
+
+    socket.onclose = () => {
+        console.log("Closed");
+    }
+
+    socket.onopen = () => {
+      new QWebChannel(socket, (channel) => {
+        console.log(channel);
+        window.qt_object = channel.objects.qt_object;
+        console.log("I OPENED");
+
+      });
+    }
+  },[]);
+
   return <>
       <div className="root">
         <Login />
