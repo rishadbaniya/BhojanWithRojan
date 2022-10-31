504496662d79c3df0a27dc72276ad60153b275b2
rishadbaniya <rbdroid01@gmail.com>
2022-11-01 00:36:35 +0545

    Added header files as modules

---
+++ backend/bhojan_with_rojan/components/AddAdmin.h
@@ -0,0 +1,112 @@
+#pragma once
+#ifndef _ADDADMIN_H_
+#define _ADDADMIN_H_
+
+#include "../libs/rapidjson/document.h"
+#include "../libs/rapidjson/writer.h"
+#include "../libs/rapidjson/stringbuffer.h"
+#include <iostream>
+#include <sqlite3.h>
+#include "utils.h"
+
+using namespace rapidjson;
+using namespace std;
+
+int sqlite_exec_callback(void* isUsernameTaken, int argc, char **argv, char **azColName){
+   bool* isTaken = (bool*)isUsernameTaken;
+
+   int i;
+   for(i = 0; i < argc; i++){
+       *isTaken = true;
+   }
+   return 0;
+}
+
+// Accepts a JSON data for Add Admin and then deserializes that
+// JSON data to the fields below in the class
+class AddAdmin{
+    public:
+        string username;
+        string email;
+        string password;
+        string gender;
+        DOB dob;
+        Image image;
+         
+        AddAdmin(const char* req_json){
+
+             // Maps all the JSON data into the struct, simply put, it deserializes everything
+             Document doc;
+             doc.Parse(req_json); // NOTE : WRONG COMPILER ERROR FROM CCLS
+             this->username = doc["username"].GetString();
+             this->email = doc["email"].GetString();
+             this->password = doc["password"].GetString();
+             this->gender = doc["gender"].GetString();
+             const Value& dob_data = doc["DOB"];
+
+             DOB dob;
+             dob.day = dob_data["day"].GetInt();
+             dob.month = dob_data["month"].GetInt();
+             dob.year = dob_data["year"].GetInt();
+             this->dob = dob;
+
+             Image image;
+             image.image_data = doc["image_data"].GetString();
+             image.file_name = doc["file_name"].GetString();
+             this->image = image;
+        }
+
+
+         // Checks if any admin exists with given username
+         bool isUsernameTaken(const char* username, sqlite3* db){
+             string query = "SELECT username FROM TABLE_NAME WHERE username='USERNAME'";
+             query.replace(query.find("TABLE_NAME"), strlen("TABLE_NAME"), ADMIN_TABLE);
+             query.replace(query.find("USERNAME"), strlen("USERNAME"), username);
+         
+             bool isUserNameTaken = false;
+             sqlite3_exec(db, query.c_str(), sqlite_exec_callback, &isUserNameTaken, NULL);
+             return isUserNameTaken;
+         
+         }
+
+         // Tries to add the new user to the database and returns true if succes
+         int addToDatabase(){
+             // In order to add any new admin to the database, one must check if a admin with same username
+             // exists there in the database already or not
+             //
+             // If an admin with same username exists there on the database then one must respond with error to
+             // the client
+         
+             sqlite3* db;
+             int isConnected = sqlite3_open(DB.c_str(), &db);
+             if(isConnected == SQLITE_OK){
+                 if(!isUsernameTaken(username.c_str(), db)){
+                     cout << "The username was not taken" << endl;
+                     string query = "INSERT INTO TABLE_NAME VALUES ('USERNAME', 'EMAIL', 'PASSWORD', 'GENDER' ,'DOB', 'IMAGE_PATH')";
+                     query.replace(query.find("TABLE_NAME"), strlen("TABLE_NAME"), ADMIN_TABLE);
+                     query.replace(query.find("USERNAME"), strlen("USERNAME"), username);
+                     query.replace(query.find("EMAIL"), strlen("EMAIL"), username);
+                     query.replace(query.find("PASSWORD"), strlen("PASSWORD"), username);
+                     query.replace(query.find("GENDER"), strlen("GENDER"), gender);
+                     query.replace(query.find("DOB"), strlen("DOB"), gender);
+                     query.replace(query.find("IMAGE_PATH"), strlen("IMAGE_PATH"), gender);
+         
+                     //string query = "INSERT INTO " + ADMIN_TABLE + " VALUES (" + username + "," + email + "," + password + "," + gender + "," + "NULL" + "," + "NULL" ;
+                     sqlite3_exec(db, query.c_str(), NULL, 0, NULL);
+                     //cout << *abc << endl;
+                     sqlite3_close(db);
+                     return OK;
+                 }else{
+                     sqlite3_close(db);
+                     cout << "The username was taken" << endl;
+                     return ERR_ADMIN_USERNAME_WAS_TAKEN;
+                 }
+             }else{
+                 sqlite3_close(db);
+                 // Can't add to the database and returns false
+                 return ERR_DATABASE_ERROR_OCCURED;
+             }
+         
+         }
+};
+#endif
--- backend/bhojan_with_rojan/controllers/AdminController.cc
+++ backend/bhojan_with_rojan/controllers/AdminController.cc
@@ -2,89 +2,14 @@
 #include "../libs/rapidjson/document.h"
 #include "../libs/rapidjson/writer.h"
 #include "../libs/rapidjson/stringbuffer.h"
+#include "../components/AddAdmin.h"
 #include <iostream>
 #include <sqlite3.h>
 
+
+#include "../components/utils.h"
+
 using namespace std;
-using namespace rapidjson;
-
-const string DB_NAME = "./test.db";
-const string ADMIN_TABLE = "admin";
-
-typedef struct {
-    int month;
-    int year;
-    int day;
-} DOB;
-
-typedef struct {
-    string image_data;
-    string file_name;
-} Image;
-
-// Accepts a JSON data for Add Admin and then deserializes that
-// JSON data to the fields below in the class
-class AddAdminData{
-
-    public:
-        string username;
-        string email;
-        string password;
-        string gender;
-        DOB dob;
-        Image image;
-         
-        AddAdminData(const char* req_json){
-            // Maps all the JSON data into the struct, simply put, it deserializes everything
-            Document doc;
-            doc.Parse(req_json); // NOTE : WRONG COMPILER ERROR FROM CCLS
-            this->username = doc["username"].GetString();
-            this->email = doc["email"].GetString();
-            this->password = doc["password"].GetString();
-            this->gender = doc["gender"].GetString();
-            //const Value& dob_data = doc["DOB"];
-
-            //DOB dob;
-            //dob.day = dob_data["day"].GetInt();
-            //dob.month = dob_data["month"].GetInt();
-            //dob.year = dob_data["year"].GetInt();
-            //this->dob = dob;
-
-            //Image image;
-            //image.image_data = doc["image_data"].GetString();
-            //image.file_name = doc["file_name"].GetString();
-            //this->image = image;
-        }
-
-        // Tries to add the new user to the database and returns true if succes
-        bool addToDatabase(){
-
-            sqlite3* db;
-            int isConnected = sqlite3_open(DB_NAME.c_str(), &db);
-
-            char* abc[1000];
-            if(isConnected == SQLITE_OK){
-                string query = "INSERT INTO TABLE_NAME "
-                 "VALUES ('USERNAME', 'EMAIL', 'PASSWORD', 'M' ,NULL, NULL)";
-                query.replace(query.find("TABLE_NAME"), strlen("TABLE_NAME"), ADMIN_TABLE);
-                query.replace(query.find("USERNAME"), strlen("USERNAME"), username);
-                query.replace(query.find("EMAIL"), strlen("EMAIL"), username);
-                query.replace(query.find("PASSWORD"), strlen("PASSWORD"), username);
-
-                cout << query << std::endl;
-
-                //string query = "INSERT INTO " + ADMIN_TABLE + " VALUES (" + username + "," + email + "," + password + "," + gender + "," + "NULL" + "," + "NULL" ;
-                sqlite3_exec(db, query.c_str(), NULL, 0, abc);
-                cout << *abc << endl;
-                sqlite3_close(db);
-                return true;
-            }else{
-                // Can't add to the database and returns false
-                return false;
-            }
-
-        }
-};
 
 // Admin URL Paths For Post Request
 const string ADMIN_LOGIN = "/admin_login";
@@ -97,14 +22,24 @@ const string EDIT_STAFF = "/edit_staff";
 const string ADD_EDIT_FOOD = "/add_edit_food";
 
 void AdminController::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback){ 
-    auto resp= drogon::HttpResponse::newHttpResponse(); // WRONG COMPILER ERROR
+    auto resp= drogon::HttpResponse::newHttpResponse(); 
     resp->setStatusCode(k200OK);
     resp->setContentTypeCode(CT_TEXT_PLAIN);
 
     if(req->path() == ADMIN_LOGIN){
     }else if(req->path() == ADD_ADMIN){
-        AddAdminData admin_data = AddAdminData(req->bodyData());
-        resp->setBody(admin_data.addToDatabase() ? "1" : "0");
+        AddAdmin add_admin = AddAdmin(req->bodyData());
+        switch(add_admin.addToDatabase()){
+            case ERR_ADMIN_USERNAME_WAS_TAKEN : 
+                resp->setBody("USERNAME_WAS_TAKEN");
+                break;
+            case ERR_DATABASE_ERROR_OCCURED : 
+                resp->setBody("DATABASE_ERROR_OCCURED");
+                break;
+            default : 
+                resp->setBody("OK");
+                break;
+        }
     }else if(req->path() == EDIT_ADMIN){
         std::cout << "edit admin" << std::endl;
     }else if(req->path() == ADD_USER){
@@ -120,6 +55,7 @@ void AdminController::asyncHandleHttpRequest(const HttpRequestPtr& req, std::fun
     }else{
         resp->setBody("BAD_REQUEST");
     }
+
     callback(resp);
 }
 
